10f7ffac9f200ceb4edfe26c8a12d383e1cbda70
yongbum94 <167834725+yongbum94@users.noreply.github.com>
2024-07-20 17:30:18 +0900

    입력값을 검증합니다

--- holiday.js
+++ holiday.js
@@ -1,1 +1,12 @@
+function run() {
+  const [code, years] = process.argv.slice(2);
+  if (typeof code !== "string") {
+    return console.error("국가코드는 필수 입력입니다.");
+  }
 
+  if (typeof years !== "string") {
+    return console.error("연도는 필수 입력입니다.");
+  }
+}
+
+run();
