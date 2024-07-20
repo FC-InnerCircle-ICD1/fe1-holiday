d8d66fd047665fa4e223edca94a994447011cbcb
kmoh0306 <36871789+kmoh0306@users.noreply.github.com>
2024-07-20 18:30:22 +0900

    next 조건 처리

--- holiday.js
+++ holiday.js
@@ -4,13 +4,30 @@ async function getPublicHolidays(countryCode, year) {
         console.error('Country code and year are required.');
         return;
     }
+    else if(countryCode.length !== 2){
+        console.error('Wrong country code');
+        return;
+    }
+        
 
 fetch(`https://date.nager.at/api/v3/PublicHolidays/${year}/${countryCode}`)
     .then(response => response.json())
     .then(data => {
-        const holidays = data;
-        holidays.forEach(holiday => {
-            console.log(holiday.name);
+        if(data.status === 404){
+            console.error('Country code not found');
+            return;
+        } else if (data.status === 400) {
+            console.error('Year not found');
+            return;
+        }
+        data.forEach(holiday => {
+            if(nextYn) {
+                const holidayDate = new Date(holiday.date);
+                if (holidayDate > today) {
+                    console.log(holiday.name);
+                }
+            } else 
+                console.log(holiday.name);
         });
     })
     .catch(error => {
@@ -21,7 +38,17 @@ fetch(`https://date.nager.at/api/v3/PublicHolidays/${year}/${countryCode}`)
 }
 
 const countryCode = process.argv[2];
-const year = process.argv[3];
+const today = new Date();
+let year = process.argv[3];
+let nextYn = false;
+
+
+if (year === "next") {
+    var d = new Date();
+    year = d.getFullYear();
+    nextYn = true;
+}
+
 getPublicHolidays(countryCode, year);
     
     
