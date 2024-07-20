01d6fb1b76efd86d0e1f0e5b635a4776dd7ab70f
이승준 <nate199458@gmail.com>
2024-07-20 16:48:55 +0900

    feat: fetch api

--- holiday.js
+++ holiday.js
@@ -1,1 +1,20 @@
+const BASE_URL = 'https://date.nager.at/api/v3/';
+
 const [country, year] = process.argv.slice(2);
+
+const fetchHolidays = async (country, year) => {
+  const response = await fetch(`${BASE_URL}PublicHolidays/${year}/${country}`);
+  return response.json();
+};
+
+// 메인 함수
+const main = async () => {
+  try {
+    const holidays = await fetchHolidays(country, year);
+    console.log(holidays);
+  } catch (error) {
+    console.error('Error fetching holidays:', error);
+  }
+};
+
+main();
