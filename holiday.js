ed5c4ef36cef55ad9c03323801b6c980174a65cb
Geunbaek <rmsqor103@naver.com>
2024-07-20 20:13:27 +0900

    refactor: holiday.js 리팩토링

--- holiday.js
+++ holiday.js
@@ -1,39 +1,42 @@
 import { getHolidays, getNextHolidays } from './apis/holidays.js';
-import { formatHolidayToString, getArguments } from './utils/helper.js';
+import { ERROR } from './contant/error.js';
+import { getArguments } from './utils/arguments.js';
+import { formatHoliday } from './utils/format.js';
+import { checkValidCountryCode } from './utils/validate.js';
 
 const main = async () => {
   const args = getArguments();
 
   if (args.length < 2) {
-    console.log(
-      'node holiday.js 국가코드 연도_또는_next 와 같은 형태로 입력해주세요.'
-    );
+    console.log(ERROR.INVALID_USAGE);
     return;
   }
 
   const [countryCode, year] = args;
 
+  const isValidCountryCode = await checkValidCountryCode({
+    countryCode: countryCode.toUpperCase(),
+  });
+
+  if (!isValidCountryCode) {
+    console.log(ERROR.INVALID_COUNTRY_CODE);
+    return;
+  }
+
   const isNext = year === 'next';
 
+  let holidays;
   try {
-    const holidays = isNext
+    holidays = isNext
       ? await getNextHolidays({ countryCode })
       : await getHolidays({ countryCode, year });
-
-    const formatedHolidays = holidays.map((holiday) =>
-      formatHolidayToString(holiday)
-    );
-
-    console.log(formatedHolidays.join('\n'));
   } catch (error) {
-    if (error.message === 'Not Found') {
-      console.log('잘못된 국가코드를 입력하셨습니다.');
-    }
-
-    if (error.message === 'Bad Request') {
-      console.log('잘못된 년도를 입력하셨습니다.');
-    }
+    console.log(error.message);
   }
+
+  const formatedHolidays = holidays.map(formatHoliday);
+
+  console.log(formatedHolidays.join('\n'));
 };
 
 main();
