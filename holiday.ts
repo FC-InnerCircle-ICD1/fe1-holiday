b54cf5da5d3cd04278e771c5eb1757e200dec8f9
silveriron <kichul7493@gmail.com>
2024-07-20 18:47:46 +0900

    refactor: 국가코드 변수명 수정, 국가코드 검증 함수 수정, 일부 코드명 수정

--- holiday.ts
+++ holiday.ts
@@ -12,9 +12,11 @@ type Holiday = {
 
 const HOLIDAY_API = "https://date.nager.at/api/v3";
 
-const getYearsHoliday = async (year: string, country: string) => {
+const getYearsHolidays = async (year: string, countryCode: string) => {
   try {
-    const res = await fetch(`${HOLIDAY_API}/publicholidays/${year}/${country}`);
+    const res = await fetch(
+      `${HOLIDAY_API}/publicholidays/${year}/${countryCode}`
+    );
     const holidays = (await res.json()) as Holiday[];
 
     return holidays;
@@ -25,9 +27,9 @@ const getYearsHoliday = async (year: string, country: string) => {
   }
 };
 
-const getNextHoliday = async (country: string) => {
+const getNextHolidays = async (countryCode: string) => {
   try {
-    const res = await fetch(`${HOLIDAY_API}/NextPublicHolidays/${country}`);
+    const res = await fetch(`${HOLIDAY_API}/NextPublicHolidays/${countryCode}`);
     const holidays = (await res.json()) as Holiday[];
     return holidays;
   } catch (e) {
@@ -43,14 +45,14 @@ const printHolidays = (holidays: Holiday[]) => {
   });
 };
 
-const printAllYearsHoliday = async (year: string, country: string) => {
-  const holidays = await getYearsHoliday(year, country);
+const printAllYearsHolidays = async (year: string, countryCode: string) => {
+  const holidays = await getYearsHolidays(year, countryCode);
   printHolidays(holidays);
 };
 
-const printYearsNextHoliday = async (country: string) => {
+const printYearsNextHolidays = async (countryCode: string) => {
   try {
-    const nextHoliday = await getNextHoliday(country);
+    const nextHoliday = await getNextHolidays(countryCode);
 
     printHolidays(nextHoliday);
   } catch (e) {
@@ -59,28 +61,25 @@ const printYearsNextHoliday = async (country: string) => {
   }
 };
 
-const validateCountry = async (country: string) => {
-  const res = await fetch(HOLIDAY_API + `/CountryInfo/${country}`);
+const isValidateCountryCode = async (countryCode: string) => {
+  const res = await fetch(HOLIDAY_API + `/CountryInfo/${countryCode}`);
 
   if (res.status === 404) {
-    console.log("Wrong country code");
-    return;
+    return false;
   }
+
+  return true;
 };
 
 const main = async () => {
-  // process.argv 배열을 통해 명령줄 인수를 가져옵니다.
-  const args = process.argv.slice(2); // 첫 두 개의 요소를 제외한 나머지 요소들을 가져옵니다.
+  const [countryCode, year] = process.argv.slice(2);
 
-  // 명령줄 인수들을 변수에 할당합니다.
-  const [country, year] = args;
-
-  if (!country) {
+  if (!countryCode) {
     console.log("국가코드를 입력해주세요.");
     return;
   }
 
-  if (country.length !== 2) {
+  if (countryCode.length !== 2) {
     console.log("국가코드는 2자리로 입력해주세요.");
     return;
   }
@@ -90,12 +89,14 @@ const main = async () => {
     return;
   }
 
-  await validateCountry(country);
+  if (!(await isValidateCountryCode(countryCode))) {
+    return console.log("wrong country code");
+  }
 
   if (year === "next") {
-    printYearsNextHoliday(country);
+    printYearsNextHolidays(countryCode);
   } else {
-    printAllYearsHoliday(year, country);
+    printAllYearsHolidays(year, countryCode);
   }
 };
 
