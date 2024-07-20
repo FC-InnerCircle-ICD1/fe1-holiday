3db471bcf94fb4026b6d1a922f8dc70306417716
2scent <ad7549@gmail.com>
2024-07-20 16:51:30 +0900

    국가코드가 올바른지 확인

--- holiday.js
+++ holiday.js
@@ -1,10 +1,24 @@
+const API_HOST = "https://date.nager.at";
+
 const EXPECTED_ARGV_LENGTH = 4;
 
-const main = () => {
+const validateCountryCode = async (countryCode) => {
+  const result = await fetch(`${API_HOST}/api/v3/CountryInfo/${countryCode}`);
+  return result.ok;
+};
+
+const main = async () => {
   if (process.argv.length !== EXPECTED_ARGV_LENGTH) {
     console.error(
       '"node holiday.js 국가코드 연도_또는_next" 형식으로 입력해주세요.'
     );
+    return;
+  }
+
+  const [, , countryCode, year] = process.argv;
+
+  if (!(await validateCountryCode(countryCode))) {
+    console.error("Wrong country code");
     return;
   }
 };
