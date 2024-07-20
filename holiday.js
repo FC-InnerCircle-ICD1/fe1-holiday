6f4c37ca656d8f0e172f7e0b96d2cab71bc3196b
Seung-wan <tmddhks0104@gmail.com>
2024-07-20 16:56:47 +0900

    유효한 국가 코드를 검사하는 코드 조각을 주석으로 추가합니다.

--- holiday.js
+++ holiday.js
@@ -1,11 +1,23 @@
-const [, , countryCode, year] = process.argv;
+async function execute() {
+  const [, , countryCode, year] = process.argv;
 
-if (!countryCode) {
-  console.error('프로그램이 종료됩니다. 이유: countryCode를 입력해주세요.');
-  process.exit();
+  if (!countryCode) {
+    console.error('프로그램이 종료됩니다. 이유: countryCode를 입력해주세요.');
+    process.exit();
+  }
+
+  if (!year) {
+    console.error('프로그램이 종료됩니다. 이유: year를 입력해주세요.');
+    process.exit();
+  }
+
+  // TODO: 유효한 국가 코드 검사
+  // if (isValidCountryCode(countryCode)) {
+  //   console.error(
+  //     '프로그램이 종료됩니다. 이유: countryCode가 유효하지 않습니다.'
+  //   );
+  //   process.exit();
+  // }
 }
 
-if (!year) {
-  console.error('프로그램이 종료됩니다. 이유: year를 입력해주세요.');
-  process.exit();
-}
+execute();
