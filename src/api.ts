301106c630f2c9dea9a02ded68c2a0c9ffaf6f73
AyoungWon <wayinwayne@gmail.com>
2024-07-20 17:34:43 +0900

    feat: remove axios

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 const BASE_URL = "https://date.nager.at/api/v3/publicholidays";
 
 //return type 수정
@@ -7,9 +5,14 @@ export const getHolidays = async (
   year: string,
   countryCode: string
 ): Promise<any> => {
+  const url = `${BASE_URL}/${year}/${countryCode}`;
   try {
-    const response = await axios.get(`${BASE_URL}/${year}/${countryCode}`);
-    return response.data;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error fetching holidays: ${response.statusText}`);
+    }
+    const holidays = await response.json();
+    return holidays;
   } catch (error) {
     console.error("Error fetching holidays:", error);
     throw error;
