4c6c4721680f070416e61d717470829ca6c08ac3
hyejin choi <chj942@gmail.com>
2024-07-20 19:40:20 +0900

    함수명 변경

--- holiday.js
+++ holiday.js
@@ -8,28 +8,9 @@ if (!countryCode || !yearOrNext) {
   process.exit(1);
 }
 
-const countryCodeValidation = async (countryCode) => {
-  const url = `${API_URL}/CountryInfo/${countryCode}`;
-  const response = await fetch(url);
-  if (response.ok) {
-    return await response.json();
-  } else {
-    console.error(
-      `Error: Wrong country code. Input countryCode: ${countryCode}`
-    );
-    process.exit(1);
-  }
-};
-
-const printHolidays = (holidays) => {
-  holidays.forEach((v) => {
-    console.log(`${v.date} ${v.localName} ${v.name}`);
-  });
-};
-
 const getPublicHolidays = async (countryCode, yearOrNext) => {
   const uppercaseCountryCode = countryCode.toUpperCase();
-  countryCodeValidation(uppercaseCountryCode);
+  validateCountryCode(uppercaseCountryCode);
   if (yearOrNext.toLowerCase() === "next") {
     const url = `${API_URL}/NextPublicHolidays/${uppercaseCountryCode}`;
     const response = await fetch(url);
@@ -49,6 +30,23 @@ const getPublicHolidays = async (countryCode, yearOrNext) => {
       console.error(`Error: ${response.title}`);
     }
   }
+};
+
+const validateCountryCode = async (countryCode) => {
+  const url = `${API_URL}/CountryInfo/${countryCode}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    console.error(
+      `Error: Wrong country code. Input countryCode: ${countryCode}`
+    );
+    process.exit(1);
+  }
+};
+
+const printHolidays = (holidays) => {
+  holidays.forEach((v) => {
+    console.log(`${v.date} ${v.localName} ${v.name}`);
+  });
 };
 
 getPublicHolidays(countryCode, yearOrNext);
