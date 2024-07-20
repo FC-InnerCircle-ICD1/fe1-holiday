2797f3d5a88c061e8be81796e86a05d8929770c2
이승준 <nate199458@gmail.com>
2024-07-20 16:57:48 +0900

    feat : 인자 갯수 검사

--- holiday.js
+++ holiday.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://date.nager.at/api/v3/';
 
-const [country, year] = process.argv.slice(2);
+const inputs = process.argv.slice(2);
 
 const fetchHolidays = async (country, year) => {
   const response = await fetch(`${BASE_URL}PublicHolidays/${year}/${country}`);
@@ -8,13 +8,19 @@ const fetchHolidays = async (country, year) => {
 };
 
 // 메인 함수
-const main = async () => {
+const main = async (country, year) => {
   try {
     const holidays = await fetchHolidays(country, year);
     console.log(holidays);
   } catch (error) {
-    console.error('Error fetching holidays:', error);
+    console.erxror('Error fetching holidays:', error);
   }
 };
 
-main();
+if (inputs.length != 2) {
+  console.log(
+    '충분한 인수가 전달되지 않았습니다. 첫번째 인자는 국가코드, 두번째는 년도나 next값을 전달해주세요'
+  );
+} else {
+  main(...inputs);
+}
