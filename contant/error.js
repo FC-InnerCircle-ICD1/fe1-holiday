8b83c47833f9d075ec06ac1d10c0441a0ae2fde1
Geunbaek <rmsqor103@naver.com>
2024-07-20 20:11:26 +0900

    feat: 상수파일 분리

---
+++ contant/error.js
@@ -0,0 +1,9 @@
+const ERROR = {
+  INVALID_REQUEST: '잘못된 요청입니다.',
+  INVALID_COUNTRY_CODE: '잘못된 국가코드 입니다.',
+  INVALID_YEAR: '잘못된 연도 입니다.',
+  INVALID_USAGE:
+    'node holiday.js 국가코드 연도_또는_next 와 같은 형태로 입력해주세요.',
+};
+
+export { ERROR };
