27ccfbc891f1ce1660e0c8b67f2677edf82d5b62
yongbum94 <167834725+yongbum94@users.noreply.github.com>
2024-07-20 17:43:16 +0900

    조회하고 에러를 출력합니다

--- holiday.js
+++ holiday.js
@@ -1,11 +1,27 @@
-function run() {
-  const [code, years] = process.argv.slice(2);
-  if (typeof code !== "string") {
+async function run() {
+  const [countryCode, years] = process.argv.slice(2);
+  if (typeof countryCode !== "string") {
     return console.error("국가코드는 필수 입력입니다.");
   }
 
   if (typeof years !== "string") {
     return console.error("연도는 필수 입력입니다.");
+  }
+
+  const res = await fetch(
+    `https://date.nager.at/api/v3/PublicHolidays/${years}/${countryCode}`
+  );
+  const body = await res.json();
+  if (res.status === 404) {
+    return console.error("Wrong country code");
+  }
+  if (res.status === 400) {
+    return console.error(body.errors.year.join("\n"));
+  }
+  if (res.status === 200) {
+    body.map(({ date, name, localName }) =>
+      console.log(`${date} ${name} ${localName}`)
+    );
   }
 }
 
