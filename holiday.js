f959224a5ba1b62b50ea5030bbd375846e5bf7b9
김민우 <0chickim@gmail.com>
2024-07-20 17:21:32 +0900

    refactor: input을 체크하는 함수 추가

--- holiday.js
+++ holiday.js
@@ -1,302 +1,53 @@
-const https = require("https");
 const HOLIDAY_API_URL = "https://date.nager.at/api/v3";
-
-const validCountryCodes = [
-  "AF",
-  "AX",
-  "AL",
-  "DZ",
-  "AS",
-  "AD",
-  "AO",
-  "AI",
-  "AQ",
-  "AG",
-  "AR",
-  "AM",
-  "AW",
-  "AU",
-  "AT",
-  "AZ",
-  "BS",
-  "BH",
-  "BD",
-  "BB",
-  "BY",
-  "BE",
-  "BZ",
-  "BJ",
-  "BM",
-  "BT",
-  "BO",
-  "BQ",
-  "BA",
-  "BW",
-  "BV",
-  "BR",
-  "IO",
-  "BN",
-  "BG",
-  "BF",
-  "BI",
-  "CV",
-  "KH",
-  "CM",
-  "CA",
-  "KY",
-  "CF",
-  "TD",
-  "CL",
-  "CN",
-  "CX",
-  "CC",
-  "CO",
-  "KM",
-  "CG",
-  "CD",
-  "CK",
-  "CR",
-  "CI",
-  "HR",
-  "CU",
-  "CW",
-  "CY",
-  "CZ",
-  "DK",
-  "DJ",
-  "DM",
-  "DO",
-  "EC",
-  "EG",
-  "SV",
-  "GQ",
-  "ER",
-  "EE",
-  "SZ",
-  "ET",
-  "FK",
-  "FO",
-  "FJ",
-  "FI",
-  "FR",
-  "GF",
-  "PF",
-  "TF",
-  "GA",
-  "GM",
-  "GE",
-  "DE",
-  "GH",
-  "GI",
-  "GR",
-  "GL",
-  "GD",
-  "GP",
-  "GU",
-  "GT",
-  "GG",
-  "GN",
-  "GW",
-  "GY",
-  "HT",
-  "HM",
-  "VA",
-  "HN",
-  "HK",
-  "HU",
-  "IS",
-  "IN",
-  "ID",
-  "IR",
-  "IQ",
-  "IE",
-  "IM",
-  "IL",
-  "IT",
-  "JM",
-  "JP",
-  "JE",
-  "JO",
-  "KZ",
-  "KE",
-  "KI",
-  "KP",
-  "KR",
-  "KW",
-  "KG",
-  "LA",
-  "LV",
-  "LB",
-  "LS",
-  "LR",
-  "LY",
-  "LI",
-  "LT",
-  "LU",
-  "MO",
-  "MG",
-  "MW",
-  "MY",
-  "MV",
-  "ML",
-  "MT",
-  "MH",
-  "MQ",
-  "MR",
-  "MU",
-  "YT",
-  "MX",
-  "FM",
-  "MD",
-  "MC",
-  "MN",
-  "ME",
-  "MS",
-  "MA",
-  "MZ",
-  "MM",
-  "NA",
-  "NR",
-  "NP",
-  "NL",
-  "NC",
-  "NZ",
-  "NI",
-  "NE",
-  "NG",
-  "NU",
-  "NF",
-  "MK",
-  "MP",
-  "NO",
-  "OM",
-  "PK",
-  "PW",
-  "PS",
-  "PA",
-  "PG",
-  "PY",
-  "PE",
-  "PH",
-  "PN",
-  "PL",
-  "PT",
-  "PR",
-  "QA",
-  "RE",
-  "RO",
-  "RU",
-  "RW",
-  "BL",
-  "SH",
-  "KN",
-  "LC",
-  "MF",
-  "PM",
-  "VC",
-  "WS",
-  "SM",
-  "ST",
-  "SA",
-  "SN",
-  "RS",
-  "SC",
-  "SL",
-  "SG",
-  "SX",
-  "SK",
-  "SI",
-  "SB",
-  "SO",
-  "ZA",
-  "GS",
-  "SS",
-  "ES",
-  "LK",
-  "SD",
-  "SR",
-  "SJ",
-  "SE",
-  "CH",
-  "SY",
-  "TW",
-  "TJ",
-  "TZ",
-  "TH",
-  "TL",
-  "TG",
-  "TK",
-  "TO",
-  "TT",
-  "TN",
-  "TR",
-  "TM",
-  "TC",
-  "TV",
-  "UG",
-  "UA",
-  "AE",
-  "GB",
-  "US",
-  "UM",
-  "UY",
-  "UZ",
-  "VU",
-  "VE",
-  "VN",
-  "VG",
-  "VI",
-  "WF",
-  "EH",
-  "YE",
-  "ZM",
-  "ZW",
-];
-
 const [, , countryCodeInput, yearOrNext] = process.argv;
 
-if (!countryCodeInput || !yearOrNext) {
-  console.error(
-    "Please provide a country code and a year or 'next' as arguments"
-  );
-  process.exit(1);
-}
+const checkInputValues = (countryCodeInput, yearOrNext) => {
+  if (countryCodeInput === undefined || yearOrNext === undefined) {
+    console.error(
+      "Please provide a country code and a year or 'next' as arguments"
+    );
+    process.exit(1);
+  }
 
-if (!validCountryCodes.includes(countryCodeInput.toUpperCase())) {
-  console.error("Invalid country code");
-  process.exit(1);
-}
+  if (countryCodeInput.length !== 2) {
+    console.error("Country code must be 2 characters long");
+    process.exit(1);
+  }
+};
 
+const checkYearOrNext = (yearOrNext) => {
+  const isNext =
+    isNaN(parseInt(yearOrNext)) &&
+    String(yearOrNext).toLocaleLowerCase() === "next";
+  const year = isNext ? new Date().getFullYear() : parseInt(yearOrNext);
+
+  return year;
+};
+
+checkInputValues(countryCodeInput, yearOrNext);
+
+const year = checkYearOrNext(yearOrNext);
 const countryCode = countryCodeInput.toUpperCase();
-const isNext =
-  isNaN(parseInt(yearOrNext)) &&
-  String(yearOrNext).toLocaleLowerCase() === "next";
 const TODAY = new Date();
-const year = isNext ? new Date().getFullYear() : parseInt(yearOrNext);
 
-const fetchHolidayYear = (_country, _year) => {
-  return new Promise((resolve, reject) => {
-    https
-      .get(`${HOLIDAY_API_URL}/PublicHolidays/${_year}/${_country}`, (resp) => {
-        let data = "";
+const fetchHolidayYear = async (_country, _year) => {
+  const fetchUrl = `${HOLIDAY_API_URL}/PublicHolidays/${_year}/${_country}`;
+  const res = await fetch(fetchUrl);
 
-        resp.on("data", (chunk) => {
-          data += chunk;
-        });
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status} ${res}`);
+  }
 
-        resp.on("end", () => {
-          const holidays = JSON.parse(data);
-          resolve(holidays);
-        });
-      })
-      .on("error", (err) => {
-        reject(err);
-      });
-  });
+  return res.json();
 };
 
 fetchHolidayYear(countryCode, year)
   .then((holidays) => {
+    if (holidays.length === 0) {
+      console.log(`No holidays found for ${countryCode} in ${year}`);
+      process.exit(0);
+    }
+
     return holidays
       .map((holiday) => {
         if (new Date(holiday.date).getTime() <= TODAY.getTime()) {
