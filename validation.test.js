ea226f107c92bf1e45e0ac5135682c941410575f
2scent <ad7549@gmail.com>
2024-07-24 23:45:54 +0900

    isInteger 함수 테스트 test runner 적용

--- validation.test.js
+++ validation.test.js
@@ -1,27 +1,30 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+
 import { isInteger } from "./validation.js";
 
-console.log("isInteger는");
+describe('isInteger는', (t) => {
+  it('빈 문자열을 입력하면, false를 리턴한다.', () => {
+    const result = isInteger('');
 
-console.log(
-  "\t빈 문자열을 입력하면, false를 리턴한다.",
-  "result:",
-  isInteger("")
-);
+    assert.strictEqual(result, false);
+  });
 
-console.log(
-  "\t'2024'를 입력하면, true를 리턴한다.",
-  "result:",
-  isInteger("2024")
-);
+  it('"2024"를 입력하면, true를 리턴한다.', () => {
+    const result = isInteger("2024");
 
-console.log(
-  "\t'-2024'를 입력하면, true를 리턴한다.",
-  "result:",
-  isInteger("-2024")
-);
+    assert.strictEqual(result, true);
+  });
 
-console.log(
-  "\t'02024'를 입력하면, false를 리턴한다.",
-  "result:",
-  isInteger("02024")
-);
+  it('"-2024"를 입력하면, true를 리턴한다.', () => {
+    const result = isInteger("-2024");
+
+    assert.strictEqual(result, true);
+  });
+
+  it('"02024"를 입력하면, false를 리턴한다.', () => {
+    const result = isInteger("02024");
+
+    assert.strictEqual(result, false);
+  });
+});
